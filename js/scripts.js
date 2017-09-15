1aed92fcb6f85b8ca41b0990d3c9226edceec592
lowesusannah@gmail.com <lowesusannah@gmail.com>
2017-09-15 14:42:22 -0700

    attempt to fix debugging in .js results, without success

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,44 @@
 // Business (back-end) logic:
-var cssReactPortland =
-var javaAndroidPortland =
-var rubyRailsSeattle =
-var cNetSeattle =
+//var cssReactPortland =
+//var javaAndroidPortland =
+//var rubyRailsSeattle =
+//var cNetSeattle =
 
 
 
 //User Interface (front-end) logic:
 $(document).ready(function() {
   $("form#track-suggest").submit(function(event) {
-  event.preventDefault();
-  var city = $("select#city").val());
-  var date = $("select#date").val());
+  var city = $("select#city").val();
+
+
+  var date = $("select#date").val();
   var dayOff = $("input:radio[name=day-off]:checked").val();
   var dreamJob = $("input:radio[name=dream-job]:checked").val();
   var website = $("input:radio[name=website]:checked").val();
 
-  var result =
+  //var result =
 
-  if (city === 'portland' && date === 'oct' && dayOff === 'css' && dreamJob === 'css' && website === 'css') {
+  if (city === "portland" && date === "oct" && dayOff === "css" && dreamJob === "css" && website === "css") {
     $().hide();
     $("#css-react-portland").show();
-  } else if (city === 'portland' && date === 'nov' && dayOff === 'java' && dreamJob === 'java' && website === 'java'){
+  }
+  else if (city === "portland" && date === "nov" && dayOff === "java" && dreamJob === "java" && website === "java"){
     $().hide();
     $("#java-android-portland").show();
-  } else if (city === 'seattle' && date === 'oct' && dayOff === 'ruby' && dreamJob === 'ruby' && website === 'ruby')
-    $().hide();
-    $("#ruby-rails-seattle").show();
-  } else if (city === 'seattle' && date === 'nov' && dayOff === 'c#' && dreamJob === 'c#' && website === 'c#')
-    $().hide();
-    $("#ruby-rails-seattle").show();
-  } else {
-
-
+  //} else if (city === 'seattle' && date === 'oct' && dayOff === 'ruby' && dreamJob === 'ruby' && website === 'ruby') {
+    //$().hide();
+    //$("#ruby-rails-seattle").show();
+  //} else (city === 'seattle' && date === 'nov' && dayOff === 'c#' && dreamJob === 'c#' && website === 'c#')
+    //$().hide();
+    //$("#ruby-rails-seattle").show();
+  //}
+} else {
     $().hide();
     $("#c-net-seattle").show();
   }
+  //$("#output").text(result);
 
-  $("#output").text(result);
+  event.preventDefault();
  });
 });
