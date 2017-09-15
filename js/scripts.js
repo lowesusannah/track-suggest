c30cc4c7f005d1e03ab3ff540b26273cf5332bd8
lowesusannah@gmail.com <lowesusannah@gmail.com>
2017-09-15 16:40:11 -0700

    fix bug so that last branch yeilds correct result; hide form input fields when result is displayed

--- js/scripts.js
+++ js/scripts.js
@@ -11,38 +11,51 @@ $(document).ready(function() {
   $("form#track-suggest").submit(function(event) {
       event.preventDefault();
   var city = $("select#city").val();
+alert(city)
   var date = $("select#date").val();
+alert(date)
   var dayOff = $("input:radio[name=day-off]:checked").val();
+alert(dayOff)
   var dreamJob = $("input:radio[name=dream-job]:checked").val();
+alert(dreamJob)
   var website = $("input:radio[name=website]:checked").val();
-
+alert (website)
   if (city === "portland" && date === "oct" && dayOff === "css" && dreamJob === "css" && website === "css") {
-    $("#java-android-portland, #ruby-rails-seattle, #c-net-seattle, #css-react-portland").hide();
+    $("#java-android-portland, #ruby-rails-seattle, #c-net-seattle, #css-react-portland, form#track-suggest").hide();
     $("#css-react-portland").show();
   } else if (city === "portland" && date === "nov" && dayOff === "java" && dreamJob === "java" && website === "java") {
-    $("#java-android-portland, #ruby-rails-seattle, #c-net-seattle, #css-react-portland").hide();
+    $("#java-android-portland, #ruby-rails-seattle, #c-net-seattle, #css-react-portland, form#track-suggest").hide();
     $("#java-android-portland").show();
   } else if (city === "seattle" && date === "oct" && dayOff === "ruby" && dreamJob === "ruby" && website === "ruby") {
-    $("#java-android-portland, #ruby-rails-seattle, #c-net-seattle, #css-react-portland").hide();
+    $("#java-android-portland, #ruby-rails-seattle, #c-net-seattle, #css-react-portland, form#track-suggest").hide();
     $("#ruby-rails-seattle").show();
   } else if (city === "seattle" && date === "nov" && dayOff === "c#" && dreamJob === "c#" && website === "c#") {
     $("#java-android-portland, #ruby-rails-seattle, #c-net-seattle, #css-react-portland").hide();
-    $("#c-net-seattle").show()
+    $("#c-net-seattle, form#track-suggest").show()
   } else if (dayOff === "css" && dreamJob === "css" && website === "css"){
     $("#java-android-portland, #ruby-rails-seattle, #c-net-seattle, #css-react-portland").hide();
-    $("#css-react-portland").show();
+    $("#css-react-portland, form#track-suggest").show();
   } else if (dayOff === "java" && dreamJob === "java" && website === "java"){
     $("#java-android-portland, #ruby-rails-seattle, #c-net-seattle, #css-react-portland").hide();
-    $("#java-android-portland").show();
+    $("#java-android-portland, form#track-suggest").show();
   } else if (dayOff === "ruby" && dreamJob === "ruby" && website === "ruby"){
     $("#java-android-portland, #ruby-rails-seattle, #c-net-seattle, #css-react-portland").hide();
-    $("#ruby-rails-seattle").show();
+    $("#ruby-rails-seattle, form#track-suggest").show();
   } else if (dayOff === "c#" && dreamJob === "c#" && website === "c#"){
-    $("#java-android-portland, #ruby-rails-seattle, #c-net-seattle, #css-react-portland").hide();
+    $("#java-android-portland, #ruby-rails-seattle, #c-net-seattle, #css-react-portland, form#track-suggest").hide();
     $("#c-net-seattle").show();
+  } else if (dreamJob === "c#"){
+    $("#java-android-portland, #ruby-rails-seattle, #c-net-seattle, #css-react-portland, form#track-suggest").hide();
+    $("#c-net-seattle").show();
+  } else if (dreamJob === "java"){
+    $("#java-android-portland, #ruby-rails-seattle, #c-net-seattle, #css-react-portland, form#track-suggest").hide();
+    $("#java-android-portland").show();
+  } else if (dreamJob === "ruby"){
+    $("#java-android-portland, #ruby-rails-seattle, #c-net-seattle, #css-react-portland, form#track-suggest").hide();
+    $("#ruby-rails-seattle").show();
   } else {
-    $("#java-android-portland, #ruby-rails-seattle, #c-net-seattle, #css-react-portland").hide();
-    $("#c-net-seattle").show();
+    $("#java-android-portland, #ruby-rails-seattle, #c-net-seattle, #css-react-portland, form#track-suggest").hide();
+    $("#css-react-portland").show();
   };
  });
 });
