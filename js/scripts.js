c505423a9db9a8b22ff1e030ba00861c722fe2d1
lowesusannah@gmail.com <lowesusannah@gmail.com>
2017-09-15 12:18:54 -0700

    define user-interface variables in stylesheet.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,38 @@
+// Business (back-end) logic:
+var cssReactPortland =
+var javaAndroidPortland =
+var rubyRailsSeattle =
+var cNetSeattle = 
+
+
+
+//User Interface (front-end) logic:
 $(document).ready(function() {
   $("form#track-suggest").submit(function(event) {
+  event.preventDefault();
+  var city = $("select#city").val());
+  var date = $("select#date").val());
+  var dayOff = $("input:radio[name=day-off]:checked").val();
+  var dreamJob = $("input:radio[name=dream-job]:checked").val();
+  var website = $("input:radio[name=website]:checked").val();
+
+  var result =
+
+  if (city === 'portland') {
+    $().hide();
     $("#css-react-portland").show();
 
-   event.preventDefault();
+    $().hide();
+    $("#java-android-portland").show();
 
+  } else {
+    $().hide();
+    $("#ruby-rails-seattle").show();
+
+    $().hide();
+    $("#c-net-seattle").show();
+  }
+
+  $("#output").text(result);
  });
 });
